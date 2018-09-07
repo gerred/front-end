9db8c796ae0459e34be9f78879ff454275c818b1
Gerred Dillon <hello@gerred.org>
2018-09-07 11:58:34 -0400

    another round on deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,20 +4,14 @@ set -o errexit
 set -o nounset
 # set -o xtrace
 
-echo $CA_CRT | base64 --decode > ${HOME}/ca.crt
+mkdir -p ${HOME}/.kube
+echo $KUBECONFIG | base64 --decode > ${HOME}/.kube/config
 
-kubectl config set-cluster our-k8s-cluster --embed-certs=true --server=${CLUSTER_ENDPOINT} --certificate-authority=${HOME}/ca.crt
-kubectl config set-credentials travis-echo --token=$SA_TOKEN
-kubectl config set-context travis --cluster=$CLUSTER_NAME --user=travis-echo --namespace=echo
-kubectl config use-context travis
-kubectl config current-context
-
-
-kubectl set image deployment/$REPO *=registry.cloudmule.xyz/$GROUP/$REPO:${COMMIT:0:8}
+kubectl set image deployments/$REPO *=registry.cloudmule.xyz/$GROUP/$REPO:${COMMIT:0:8} -n sock-shop
 
 function cleanup {
     printf "Cleaning up...\n"
-    rm -vf "${HOME}/ca.crt"
+    rm -vf "${HOME}/.kube/config"
     printf "Cleaning done."
 }
 
