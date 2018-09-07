8028993b8393798514ea71b99cb253879d4d6b4e
Gerred Dillon <hello@gerred.org>
2018-09-07 11:36:39 -0400

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 # set -o xtrace
 
-echo $CA_CRT | base64 --decode -i > ${HOME}/ca.crt
+echo $CA_CRT | base64 --decode > ${HOME}/ca.crt
 
 kubectl config set-cluster our-k8s-cluster --embed-certs=true --server=${CLUSTER_ENDPOINT} --certificate-authority=${HOME}/ca.crt
 kubectl config set-credentials travis-echo --token=$SA_TOKEN
